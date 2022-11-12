2c0a9a6b85ba1d500d96f7ee236945730b2766cb
Srinivasan260 <117920909+Srinivasan260@users.noreply.github.com>
2022-11-12 21:46:08 +0530

    Add files via upload

---
+++ userdetails.py
@@ -0,0 +1,9 @@
+class details:
+    firstname=str(input("enter your first name:"))
+    lastname=str(input("enter your lastname:"))
+    email=str(input("enter your email:"))
+    username=str(input("enter your username:"))
+    userid=int(input("enter your id:"))
+user=details
+a=[user.firstname,user.lastname,user.email,user.username]
+print(a)
