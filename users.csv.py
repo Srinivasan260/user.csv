6cec09a77d840ccfa3842309d094296ec9fb5477
Srinivasan260 <117920909+Srinivasan260@users.noreply.github.com>
2022-11-12 22:07:11 +0530

    Add files via upload

---
+++ users.csv.py
@@ -0,0 +1,11 @@
+class details:
+    firstname=str(input("enter your first name:"))
+    lastname=str(input("enter yourb lastname:"))
+    email=str(input("enter ypour email:"));
+    username=str(input("enter your username:"));
+    userid=int(input("enter your id:"));
+user=details
+a=[user.firstname,user.lastname,user.email,user.username]
+print(a)
+
+a.sort()print(a)a.sort()print(a)
